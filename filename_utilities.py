66b7be30e0dbcd1bc577672dd8fa30383f7ee78e
Noah Lee <iamnoah@myself.com>
2022-07-18 17:21:49 -0500

    init

---
+++ filename_utilities.py
@@ -0,0 +1,38 @@
+import re
+
+MATCHUP_REGEX = re.compile('[A-Z]+_?[A-Z]*x[A-Z]+_?[A-Z]*')
+DEFAULT_LOGGING = False
+
+class FilenameError(Exception):
+    # Raised when something is wrong with filename being parsed.
+    def __init__(self, message):
+        self.message = message
+        super.__init__(self.message)
+
+# Some char names contain '_' but if char name does not,
+# regex will capture a trailing _ that we do not want.
+def trimMatchup(matchup):
+    return matchup[:-1] if matchup.endswith('_') else matchup
+
+def getChars(matchup):
+    trimmed_matchup = trimMatchup(matchup)
+    return trimmed_matchup.split('x')
+
+def getMatchup(filename):
+    print(f'Matching {filename}')
+    match = MATCHUP_REGEX.match(filename)
+    if match == None:
+        raise FilenameError(f'Failed to match matchuup regex for file {filename}')
+    matchup = getChars(match.group())
+    return matchup
+
+# Given matchup has 2 names, return one that is not
+# the given char_name.    
+def getOpponent(matchup, char_name):
+    char1 = matchup[0]
+    char2 = matchup[1]
+    print(f'Char1 = {char1}, Char2 = {char2}, char: {char_name}')
+    if char1 != char_name and char2 != char_name:
+        return None
+    return char2 if char1 == char_name else char1
+
---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import filename_utilities
+
+def countOpponents(filenameList, char_name):
+    print(filenameList)
+    opponent_counts = {}
+    
+    for filename in filenameList:
+        matchup = filename_utilities.getMatchup(filename)
+        print(f'Found matchup {matchup}')
+        opponent_char = filename_utilities.getOpponent(matchup, char_name)
+        print(f'Opponent: {opponent_char}')
+        # None suggests neither char is one we are interested in, so we skip.
+        if opponent_char != None:
+            count = opponent_counts.get(opponent_char)
+            if count == None:
+                opponent_counts[opponent_char] = 1
+            else:
+                opponent_counts[opponent_char] += 1
+            
+    return opponent_counts
+
+def printOpponentCounts(count_dict):
+    for opp, count in count_dict.items():
+        print(f'{opp}: {count}')
+
+def main():
+    print('Please enter path to replay directory:')
+    replay_dir_path = str(input())
+    print('Please enter name of the char you play as it appears in replay filename:')
+    char_name = str(input()).upper()
+    
+    filenames = [filename for filename in os.listdir(replay_dir_path) if isfile(join(replay_dir_path, filename))]
+    
+    opponent_count_dict = countOpponents(filenames, char_name)
+    # source: https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
+    sorted_counts = dict(sorted(opponent_count_dict.items(), key=lambda item: item[1]))
+    
+    printOpponentCounts(sorted_counts)
+    
+
+######################
+
+main()
